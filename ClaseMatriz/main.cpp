aef8acd7546ad9cc6f94708565eaf652e8dfc756
FlorM <flowy6290@gmail.com>
2025-01-26 19:10:26 -0700

    Organizo el main con comentarios

--- ClaseMatriz/main.cpp
+++ ClaseMatriz/main.cpp
@@ -4,6 +4,34 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Programa que realiza operaciones de matrices" << endl;
+
+
+    //Captura de los vectores ****************************************************
+    cout << "Ingrese la cantidad de renglones que tiene la primera matriz" << endl;
+
+    cout << "Ingrese la cantidad de columnas que tiene la primera matriz" << endl;
+
+    cout << "Ingrese los componentes de la primera matriz" << endl;
+
+    cout << "Ingrese la cantidad de renglones que tiene la segunda matriz" << endl;
+
+    cout << "Ingrese la cantidad de columnas que tiene la segunda matriz" << endl;
+
+    cout << "Ingrese los componentes de la segunda matriz" << endl;
+
+    //Suma de dos matrices *********************************************************
+
+    //Resta de dos matrices ********************************************************
+
+    //Producto de dos matrices *****************************************************
+
+    //Producto de una matriz por un escalar ****************************************
+
+    //La transpuesta de una matriz *************************************************
+
+    //La inversa de una matriz cuadrada ********************************************
+
+    //Método de redimensionamiento de una matriz ********************************
     return 0;
 }
